c3ab9475467a2e4d6e9d7e664efb582e7e2b1e65
mfboulos <mboulos95@gmail.com>
2019-11-13 13:52:46 -0800

    lvl command done

--- bot/commands.go
+++ bot/commands.go
@@ -1,13 +1,68 @@
-package oziachbot
+package bot
 
 import (
-	"strings"
+	"fmt"
 
 	"github.com/gempir/go-twitch-irc"
+	"github.com/mfboulos/oziachbot/hiscores"
 )
 
+// IncorrectFormatError Returned when a command invocation is malformed
+type IncorrectFormatError struct{}
+
+func (e *IncorrectFormatError) Error() string {
+	return "Incorrect format for command"
+}
+
 // HandleSkillLookup parses user message and sends the formatted result of a skill lookup
-func (bot *OziachBot) HandleSkillLookup(channel string, user twitch.User, message twitch.Message) {
-	tokens := strings.SplitN(message.Text, " ", 3)
-	// hit the hiscores api and extract whatever is needed
+func (bot *OziachBot) HandleSkillLookup(channel string, user twitch.User, skillName, player string) error {
+	mode := hiscores.Normal
+	playerHiscores, err := hiscores.LookupHiscores(player, mode)
+
+	// All players should be in normal hiscores. If they are not, they are either unranked
+	// or they do not exist
+	if err != nil {
+		bot.Say(channel, fmt.Sprintf("@%s Could not find player %s", user.DisplayName, player))
+		return err
+	}
+
+	ironmanHiscores, err2 := hiscores.LookupHiscores(player, hiscores.Ironman)
+	if err2 == nil {
+		hardcoreHiscores, err3 := hiscores.LookupHiscores(player, hiscores.HardcoreIronman)
+		ultimateHiscores, err4 := hiscores.LookupHiscores(player, hiscores.UltimateIronman)
+
+		// To determine what kind of ironman we have, first we check to see if there is a
+		// hiscore under that GameMode. If the hiscores in that GameMode and GameMode.Ironman
+		// match, the player is in that GameMode
+		if err3 == nil && hardcoreHiscores == ironmanHiscores {
+			playerHiscores = hardcoreHiscores
+			mode = hiscores.HardcoreIronman
+		} else if err4 == nil && ultimateHiscores == ironmanHiscores {
+			playerHiscores = ultimateHiscores
+			mode = hiscores.UltimateIronman
+		} else {
+			playerHiscores = ironmanHiscores
+			mode = hiscores.Ironman
+		}
+	}
+
+	skill, err5 := playerHiscores.GetSkillHiscoreFromName(skillName)
+
+	// If the name doesn't map to a skill, the bot silently fails to retrieve it
+	if err5 != nil {
+		return err5
+	}
+
+	bot.Client.Say(channel, fmt.Sprintf(
+		"/me @%s - %s, %s level: %v, Rank (%s): %v, Exp: %v",
+		user.DisplayName,
+		player,
+		skill.Name,
+		skill.Level,
+		mode.Name,
+		skill.Rank,
+		skill.Exp,
+	))
+
+	return nil
 }
--- bot/oziach-bot.go
+++ bot/oziach-bot.go
@@ -1,6 +1,7 @@
-package oziachbot
+package bot
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/gempir/go-twitch-irc"
@@ -13,9 +14,37 @@ type OziachBot struct {
 }
 
 // HandleMessage Main callback method to wrap all actions on a PRIVMSG
-func (bot *OziachBot) HandleMessage(channel string, user twitch.User, message twitch.Message) {
-	switch strings.Fields(message.Text)[0] {
+func (bot *OziachBot) HandleMessage(channel string, user twitch.User, message twitch.Message) error {
+	switch idx := strings.Index(message.Text, " "); message.Text[:idx] {
 	case "!lvl", "!level":
-		// go bot.HandleSkillLookup(channel string, user twitch.User, message twitch.Message)
+		tokens := strings.SplitN(message.Text, " ", 3)
+		if len(tokens) < 3 {
+			return &IncorrectFormatError{}
+		}
+
+		skillName := tokens[1]
+		// Truncate player to 12 characters, max length of an OSRS username
+		player := tokens[2][:12]
+
+		go bot.HandleSkillLookup(channel, user, skillName, player)
+	case "!total", "!overall":
+		tokens := strings.SplitN(message.Text, " ", 2)
+		if len(tokens) < 2 {
+			return &IncorrectFormatError{}
+		}
+
+		skillName := "Overall"
+		// Truncate player to 12 characters, max length of an OSRS username
+		player := tokens[1][:12]
+
+		go bot.HandleSkillLookup(channel, user, skillName, player)
 	}
+
+	return nil
+}
+
+// Say Wrapper for Client.Say that prefixes the text with "/me"
+func (bot *OziachBot) Say(channel, text string) {
+	formattedText := fmt.Sprintf("/me %s", text)
+	bot.Client.Say(channel, formattedText)
 }
